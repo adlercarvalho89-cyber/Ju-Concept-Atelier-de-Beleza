7cca90aede458ce57e809b7ada572ebcad972c59
adlercarvalho89-cyber <adlercarvalho89@gmail.com>
2026-06-20 11:56:11 -0300

    "feat(navbar): cria navbar com glassmorphism, menu mobile e contador de carrinho"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import Navbar from './components/layout/Navbar';
 
+function App() {
+  return <Navbar />;
+}
 
-
-export default App
+export default App;
